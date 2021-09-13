71e31c3f11249d6368d10322c711abef11aa4ea3
TUE\20201951 <j.j.h.m.v.grinsven@student.tue.nl>
2021-09-13 11:05:46 +0200

    Flywheel constants + Flywheel mass calculation

--- main.m
+++ main.m
@@ -12,11 +12,17 @@ close all;
 time_start = 0;
 time_step = 0.25;
 time_end = 24;
+
+r_flywheel = 0.85;                                                          %Radius of the flywheel
+d_flywheel = 0.40;                                                          %Thickness of the flywheel
+rho_flywheel = 7850;                                                        %Density of the material of which the flywheel is made (solid)
+
 %%
 %%%%%%%%%%%%%%%%
 % Calculations %
 %%%%%%%%%%%%%%%%
 
+m_flywheel = pi * r_flywheel^2 * rho_flywheel;                              %Calculating the mass of the flywheel
 
 
 %%
