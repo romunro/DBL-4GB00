240ca2dd89be05f84921605d23ec85d554d3ef8f
TuLapRonan <ronan18@live.nl>
2021-09-13 11:39:47 +0200

    Added data logging on table

--- main.m
+++ main.m
@@ -9,15 +9,25 @@ close all;
 %%%%%%%%%%%%%
 % Constants %
 %%%%%%%%%%%%%
-time_start = 0;
-time_step = 0.25;
-time_end = 24;
+%%%Starting values
+time_start = 0;                                                             %Starting time
+time_step = 0.25;                                                           %Time step, every 15 minutes
+time_end = 24;                                                              %End time, after 24 hours
+flywheel_in = 0;                                                            %Starting inflow energy
+flywheel_energy = 0;                                                        %Starting energy flywheel
+flywheel_rev = 0 ;                                                          %Starting revolutions flywheel, also energy
+
 %%
 %%%%%%%%%%%%%%%%
 % Calculations %
 %%%%%%%%%%%%%%%%
+%%% Read CSV data
+table_energy = readmatrix('datasets\numericalEnergyData.csv');
 
-
+%%% Setting up tables
+Steps = time_end/time_step;                                                 %Amount of steps
+table_flywheel = zeros(3,Steps+1);                                          %Heat vessel table for data logging
+table_flywheel(1,:) = time_start:time_step:time_end;                        
 
 %%
 %%%%%%%%%%%%%%%%%%%
@@ -25,7 +35,16 @@ time_end = 24;
 %%%%%%%%%%%%%%%%%%%
 
 for t=time_start:time_step:time_end
+%Incoming energy
+    Column = round((1/time_step)*t+1);                                         %Table time step counter
 
+    flywheel_in = table_energy(2,Column);
+    flywheel_energy = flywheel_energy + flywheel_in;
+
+    %Log data to table 
+    table_flywheel(2,Column)=flywheel_rev;                                  %Assign value for flywheel revolutions to table
+    table_flywheel(3,Column)=flywheel_energy;                               %Assign value for flywheel energy to table
+    table_flywheel(4,Column)=flywheel_in;                                   %Assign value for incomming flywheel energy to table
 end
 %%
 %%%%%%%%%%%%
