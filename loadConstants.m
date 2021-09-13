293f4c67080de926fa4f90e03970314544eaa551
TuLapRonan <ronan18@live.nl>
2021-09-13 15:36:50 +0200

    Moved Constants to seperate .m file Moved graph plotting to seperate .m file Added check that flywheel can not have negative angular velocity

---
+++ loadConstants.m
@@ -0,0 +1,20 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Constants for Energy Storage System Group 21   %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+%%%%%%%%%%%%%
+% Constants %
+%%%%%%%%%%%%%
+%%%Starting values
+time_start = 0;                                                             %Starting time
+time_step = 0.25;                                                           %Time step, every 15 minutes
+time_end = 24;                                                              %End time, after 24 hours
+flywheel_in = 0;                                                            %Starting inflow energy
+flywheel_energy = 0;                                                        %Starting energy flywheel
+flywheel_angular = 0 ;                                                      %Starting angular velocity flywheel
+
+r_flywheel = 0.85;                                                          %Radius of the flywheel
+d_flywheel = 0.40;                                                          %Thickness of the flywheel
+rho_flywheel = 7850;                                                        %Density of the material of which the flywheel is made (solid)
+rho_air = 1.2041;                                                           %Density of air
+c_lin = 1;                                                                  %linear air drag coefficient
--- main.m
+++ main.m
@@ -4,22 +4,7 @@
 clc;
 clear;
 close all;
-
-%%
-%%%%%%%%%%%%%
-% Constants %
-%%%%%%%%%%%%%
-%%%Starting values
-time_start = 0;                                                             %Starting time
-time_step = 0.25;                                                           %Time step, every 15 minutes
-time_end = 24;                                                              %End time, after 24 hours
-flywheel_in = 0;                                                            %Starting inflow energy
-flywheel_energy = 0;                                                        %Starting energy flywheel
-flywheel_angular = 0 ;                                                      %Starting angular velocity flywheel
-
-r_flywheel = 0.85;                                                          %Radius of the flywheel
-d_flywheel = 0.40;                                                          %Thickness of the flywheel
-rho_flywheel = 7850;                                                        %Density of the material of which the flywheel is made (solid)
+loadConstants;
 
 %%
 %%%%%%%%%%%%%%%%
@@ -49,6 +34,10 @@ for t=time_start:time_step:time_end
     flywheel_in = table_energy(2,Column);
     flywheel_in_joule = flywheel_in*3.6e+6;
     flywheel_energy = flywheel_energy + flywheel_in;
+    
+    %flywheel_losses = pi * flywheel_angular^2 * r_flywheel^4 * d_flywheel * rho_air;
+    flywheel_losses = 0;
+    flywheel_in_joule = flywheel_in_joule - flywheel_losses;
     if flywheel_in_joule >= 0
         dt_flywheel_angular = sqrt((2 * flywheel_in_joule)/(I_flywheel));
     else
@@ -56,51 +45,21 @@ for t=time_start:time_step:time_end
         dt_flywheel_angular = -sqrt((2 * flywheel_in_joule)/(I_flywheel));
     end
     
-    %dt_flywheel_angular = sqrt((2 * flywheel_in_joule)/(I_flywheel));
     flywheel_angular = flywheel_angular + dt_flywheel_angular;
-
+    
+    if flywheel_angular < 0
+        flywheel_angular = 0;
+    end
+    
     %Log data to table 
-    table_flywheel(2,Column)=flywheel_angular;                                  %Assign value for flywheel revolutions to table
+    table_flywheel(2,Column)=flywheel_angular;                              %Assign value for flywheel revolutions to table
     table_flywheel(3,Column)=flywheel_energy;                               %Assign value for flywheel energy to table
     table_flywheel(4,Column)=flywheel_in;                                   %Assign value for incomming flywheel energy to table
+    table_flywheel(5,Column)=flywheel_losses;                               %Assign value for energy losses to table    
 end
 %%
-%%%%%%%%%%%%
-% Graphing %
-%%%%%%%%%%%%
-t_var = table_flywheel(1,:);
-Omega_flywheel = table_flywheel(2,:);
-E_energy = table_flywheel(3,:);
-E_inflow = table_flywheel(4,:);
-%%Figure 1 for surplus
-figure(1);hold on
-figure(1);grid on
-figure(1); plot(t_var,E_inflow);
+%Settings plots
+generate_fig1 = false;                                                       %Inflow energy graph
+generate_fig2 = true;                                                       %Angular velocity flywheel graph
 
-%figure(1); annotation('line',[0 1], [0.5 0.5]);
-%figure(1); annotation('textarrow', [0.45 0.33], [0.25 0.25], 'String', 'Thermocline effect');
-
-figure(1); ylabel('Inflow energy (kWh)')
-figure(1); legend({'Inflow energy flywheel'}, 'Location','northeast')
-
-figure(1); xlim([0 time_end]);
-figure(1); xlabel('Time (hours)')
-figure(1); title('Inflow energy flywheel')
-figure(1); saveas(gcf,'figures\inflowEnergy.jpg')
-
-%%Figure 1 for flywheel rotation
-figure(1);hold on
-figure(1);grid on
-figure(1); plot(t_var,Omega_flywheel);
-
-%figure(1); annotation('line',[0 1], [0.5 0.5]);
-%figure(1); annotation('textarrow', [0.45 0.33], [0.25 0.25], 'String', 'Thermocline effect');
-
-figure(1); ylabel('Angular velocity [rad/s]')
-figure(1); legend({'Angular velocity [rad/s]'}, 'Location','northeast')
-
-figure(1); xlim([0 time_end]);
-figure(1); xlabel('Time (hours)')
-figure(1); title('Angular velocity flywheel')
-figure(1); saveas(gcf,'figures\angularVelocity.jpg')
-
+plotGraphs;
